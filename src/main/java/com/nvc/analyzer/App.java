d4bef967250de802a2692d98f6a7ed86c13dbba4
Onk Ar <kocarond@gmail.com>
2026-02-06 16:29:46 +0100

    Created the first application draft

--- src/main/java/com/nvc/analyzer/App.java
+++ src/main/java/com/nvc/analyzer/App.java
@@ -1,5 +1,28 @@
 package com.nvc.analyzer;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
     
+    @Override
+    public void start(Stage stage) {
+        var label = new Label("Welcome to NVC Analyzer");
+
+        var sceneRoot = new StackPane(label);
+
+        var scene = new Scene(sceneRoot, 640, 480);
+
+        stage.setScene(scene);
+        stage.setTitle("NVC Analyzer");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        launch();
+    }
 }
