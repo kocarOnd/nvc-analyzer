8bafe228abcb89118138d9c425d051bdd7d987ee
Onk Ar <kocarond@gmail.com>
2026-02-07 15:47:59 +0100

    Created a data model class for the NVC process

---
+++ src/main/java/com/nvc/analyzer/model/NvcProcess.java
@@ -0,0 +1,56 @@
+package com.nvc.analyzer.model;
+
+public class NvcProcess {
+    private String observation;
+    private String feeling;
+    private String need;
+    private String request;
+
+    public NvcProcess() {
+        this.observation = "";
+        this.feeling = "";
+        this.need = "";
+        this.request = "";
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public String getFeeling() {
+        return feeling;
+    }
+
+    public void setFeeling(String feeling) {
+        this.feeling = feeling;
+    }
+
+    public String getNeed() {
+        return need;
+    }
+
+    public void setNeed(String need) {
+        this.need = need;
+    }
+
+    public String getRequest() {
+        return request;
+    }
+
+    public void setRequest(String request) {
+        this.request = request;
+    }
+
+    @Override
+    public String toString() {
+        return "NVC Process Summary:\n" +
+               "1. When I see/hear: " + observation + "\n" +
+               "2. I feel: " + feeling + "\n" +
+               "3. Because I need: " + need + "\n" +
+               "4. Would you be willing to: " + request;
+    }
+}
