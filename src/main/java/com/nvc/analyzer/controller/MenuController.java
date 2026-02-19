77ec5b41919871a767fd244fe600b98653f9c570
Onk Ar <kocarond@gmail.com>
2026-02-19 12:58:44 +0100

    Added Javadoc comments for public and FXML elements

--- src/main/java/com/nvc/analyzer/controller/MenuController.java
+++ src/main/java/com/nvc/analyzer/controller/MenuController.java
@@ -4,13 +4,16 @@ import com.nvc.analyzer.App;
 import javafx.fxml.FXML;
 import java.io.IOException;
 
+/**Controller for the menu view. */
 public class MenuController {
 
+    /**Triggered by the Create New Analysis button click, this method changes the current root to analysis view. */
     @FXML
     private void switchToCreate() throws IOException {
         App.setRoot("analysis_view");
     }
 
+    /**Triggered by the Browse History button click, this method changes the current root to history view. */
     @FXML
     private void switchToBrowse() throws IOException {
         App.setRoot("history_view");
